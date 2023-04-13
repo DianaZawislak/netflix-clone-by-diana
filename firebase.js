ee23427899ea4f879545b2914310be08ad930ea4
Diana Zawislak <diana.zawislak@icloud.com>
2023-04-13 13:15:37 -0400

    init set up

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCne1bjpgkmCFxyZ21BG_R8RF3jadLunkE",
+    authDomain: "netflix-clone-by-diana.firebaseapp.com",
+    projectId: "netflix-clone-by-diana",
+    storageBucket: "netflix-clone-by-diana.appspot.com",
+    messagingSenderId: "907799363746",
+    appId: "1:907799363746:web:a2a0eee84f1fedd6da17fb"
+  };
