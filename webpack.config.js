0f1c0f3d36c5e97efc2bfd0b8dd925d9c4b87aa2
Diana Zawislak <diana.zawislak@icloud.com>
2023-04-15 18:42:15 -0400

    bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+const dotenv = require('dotenv').config();
 
 module.exports = {
-    // ...
-    resolve: {
-      fallback: {
-        "fs": false,
-        "path": require.resolve("path-browserify"),
-        "os": require.resolve("os-browserify/browser")
-      }
-    },
-
-      plugins: [
-        new webpack.ProvidePlugin({
+  // ...
+  resolve: {
+    fallback: {
+      "fs": false,
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser")
+    }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
       process: 'process/browser',
+    }),
+    new Dotenv({
+      path: path.resolve(__dirname, '.env'),
+      systemvars: true,
     }),
   ],
   // other webpack configuration options
-}
+};
