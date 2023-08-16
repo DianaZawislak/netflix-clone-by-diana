e884c46a87595f7f135711ae4844a64f56964ab9
Diana Zawislak <diana.zawislak@icloud.com>
2023-08-16 18:04:27 -0400

    firebase configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore, collection, getDocs } from "firebase/firestore";
-import { getAuth, onAuthStateChanged, createUserWithEmailAndPassword  } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
@@ -20,5 +18,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 const auth = getAuth(firebaseApp);
 
-export { auth };
-export default db;
+export { auth, db };
